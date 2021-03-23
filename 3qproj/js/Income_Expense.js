67aa682253b0aa4a8ea03f75c34bcdbc28ca03e9
glnll <76585644+glnll@users.noreply.github.com>
2021-03-23 22:11:20 +0800

    Update Income_Expense.js

--- 3qproj/js/Income_Expense.js
+++ 3qproj/js/Income_Expense.js
@@ -1,3 +1,5 @@
+
+
 function submit(){
 	//make new table body and row
 	var tableBody = document.getElementById("tableBody");
@@ -8,36 +10,49 @@ function submit(){
 	var option = category.options[category.selectedIndex].text;
 
 	//create new date
+	var months = ["January", "February","March", "April", "June", "July", "August", "September", "October", "November", "December"]
+	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 	var date = new Date();
-	var month = date.getMonth() + 1;
-	var day = date.getDate();
+	var month = months[date.getMonth()];
+	var dayNum = date.getDate();
 	var year = date.getFullYear();
-	var fulldate = month + "/" + day + "/" + year;
+	var dayWord = days[date.getDay()];
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var time;
+
+	if(hour < 13){
+		time = "AM";
+	}
+	else{
+		hour -= 12;
+		time = "PM";
+	}
+
+	if(minute < 10){
+		minute = "0" + minute;
+	}
+
+	//final date
+	var fulldate = dayWord + ", " + month + " " + dayNum + ", " + year + ", " + hour + ":" + minute + " "+ time;
 
 	//create new table cells
 	var tdcategory = document.createElement("td");
 	var tdcost = document.createElement("td");
 	var tddate = document.createElement("td");
+	var total = document.createElement("td");
 
 	// assign input into created table cells above
  	tdcategory.innerHTML = option;
-	tdcost.innerHTML = document.getElementById("cost").value;
+	tdcost.innerHTML = parseFloat(document.getElementById("cost").value);
 	tddate.innerHTML = fulldate;
-
-	//compute for total IDK ANYMORE MAN
-	var total = [];
-	var add = parseFloat(document.getElementById("cost").value); // input value
-	var sum = localStorage.setItem("sum", t + add);
-	var total = sum + add;
-
-	//print total
-	document.getElementById("total").innerHTML = sum;
 
 	//add to table
 	newrow.appendChild(tdcategory);
 	newrow.appendChild(tdcost);
 	newrow.appendChild(tddate);
 	tableBody.appendChild(newrow);
-
 }
+
 
